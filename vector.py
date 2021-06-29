01d4024646039f0ba4437627d4c838cc4fea94bd
LuisaFranco23 <85638132+LuisaFranco23@users.noreply.github.com>
2021-06-28 21:29:41 -0500

    Add files via upload

---
+++ vector.py
@@ -0,0 +1,28 @@
+class deber:
+    def __init__(self):
+        pass
+
+    def vector(self):
+        print("_______________________________")
+        num[20], i, j, k, A[20], B[20] = 0, 0, 0, 0, 0, 0
+        j = 1
+        k = 1
+        for i == 1 in range(1, 20, 1):
+            num[i] = int(input("Ingrese numero: "))
+            if num[i] > 0:
+                A[j] = num[i]
+                j = j + 1
+            else:
+                B[k] = num[i]
+                k = k + 1
+        for i == 1 in j:
+            print(A[j])
+        for i == 1 in k:
+            print(B[k])
+
+        print("_______________________________")
+        input("enter para salir")
+
+
+tarea = deber()
+tarea.vector()
